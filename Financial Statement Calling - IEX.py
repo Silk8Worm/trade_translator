fed4e218b58ebbc0635a28fc46d808161ad4b120
Silk8Worm <gamorfit@gmail.com>
2020-09-13 16:10:55 -0400

    first commit

---
+++ Financial Statement Calling - IEX.py
@@ -0,0 +1,133 @@
+"""
+
+#TODO IMPORTANT: SET SANDBOX TO FALSE IF YOU WANT TO USE A REAL API TOKEN
+
+Need to "pip install requests" in command prompt and install package
+
+install by opening the Settings window, and doing the following:
+Open Project: csc148 -> Project Intepreter.
+Ensure that you have Python 3.8 selected in the “Project Intepreter” dropdown.
+Press the green “+” icon, and type in requests
+Select the package, and press “Install Package”.
+
+"""
+
+from typing import Dict, Optional
+import requests
+import json
+
+SANDBOX = True
+
+
+def cash_flow_statement(api_key: str, ticker: str, period: Optional[str] = 'quarter', num_periods: Optional[int] = 1) -> Dict:
+    """
+    Used for sending an HTTP request for the JSON response provided by IEX Cloud
+
+    api_key: must be a valid api token for IEX cloud to take
+    ticker: must be a valid ticker that exists within the iex cloud
+    period: optional parameter specifying peiod of balance sheet. Must be either
+        "annual" or "quarter"
+        - default value is quarter
+    num_periods: the number of periods that you want data for
+        - Maximum 12 quarters or 4 years
+        - default value is 1
+    """
+
+    if not SANDBOX:
+        sand = 'cloud'
+    else:
+        sand = 'sandbox'
+
+    url = f'https://{sand}.iexapis.com/stable/stock/{ticker}/cash-flow?'
+    if period == "annual":
+        url += 'period=annual&'
+    if period == "annual":
+        if num_periods < 5:
+            url += f'last={num_periods}&'
+    elif num_periods < 13:
+        url += f'last={num_periods}&'
+    else:
+        print("Cannot request that many periods. Please read method description.")
+    url += f'token={api_key}'
+
+    # r represents the data that the website returns. I can edit it/scrape it however you'd like.
+    # it is also in a JSON format which is held as a dictionary by python
+    r = requests.get(url)
+    return r.json()
+
+
+
+def balance_sheet(api_key: str, ticker: str, period: Optional[str] = 'quarter', num_periods: Optional[int] = 1) -> Dict:
+    """
+    Used for sending an HTTP request for the JSON response provided by IEX Cloud
+
+    api_key: must be a valid api token for IEX cloud to take
+    ticker: must be a valid ticker that exists within the iex cloud
+    period: optional parameter specifying peiod of balance sheet. Must be either
+        "annual" or "quarter"
+        - default value is quarter
+    num_periods: the number of periods that you want data for
+        - Maximum 12 quarters or 4 years
+        - default value is 1
+    """
+
+    if not SANDBOX:
+        sand = 'cloud'
+    else:
+        sand = 'sandbox'
+
+    url = f'https://{sand}.iexapis.com/stable/stock/{ticker}/balance-sheet?'
+    if period == "annual":
+        url += 'period=annual&'
+    if period == "annual":
+        if num_periods < 5:
+            url += f'last={num_periods}&'
+    elif num_periods < 13:
+        url += f'last={num_periods}&'
+    else:
+        print("Cannot request that many periods. Please read method description.")
+    url += f'token={api_key}'
+
+    # r represents the data that the website returns. I can edit it/scrape it however you'd like.
+    # it is also in a JSON format which is held as a dictionary by python
+    r = requests.get(url)
+    print(url)
+    return r.json()
+
+def income_statement(api_key: str, ticker: str, period: Optional[str] = 'quarter', num_periods: Optional[int] = 1) -> Dict:
+    """
+    Used for sending an HTTP request for the JSON response provided by IEX Cloud
+
+    api_key: must be a valid api token for IEX cloud to take
+    ticker: must be a valid ticker that exists within the iex cloud
+    period: optional parameter specifying peiod of balance sheet. Must be either
+        "annual" or "quarter"
+        - default value is quarter
+    num_periods: the number of periods that you want data for
+        - Maximum 12 quarters or 4 years
+        - default value is 1
+    """
+
+    if not SANDBOX:
+        sand = 'cloud'
+    else:
+        sand = 'sandbox'
+
+    url = f'https://{sand}.iexapis.com/stable/stock/{ticker}/income?'
+    if period == "annual":
+        url += 'period=annual&'
+    if period == "annual":
+        if num_periods < 5:
+            url += f'last={num_periods}&'
+    elif num_periods < 13:
+        url += f'last={num_periods}&'
+    else:
+        print("Cannot request that many periods. Please read method description.")
+    url += f'token={api_key}'
+
+    # r represents the data that the website returns. I can edit it/scrape it however you'd like.
+    # it is also in a JSON format which is held as a dictionary by python
+    r = requests.get(url)
+    return r.json()
+
+print(json.dumps(balance_sheet("Tpk_6ef4a75d2a6f4d95be047b5629cf964f", "AAPL")))
---
+++ algorithm_builder.py
@@ -0,0 +1,92 @@
+"""
+This is used for proof of concept of a boolean tree that can process a sentence
+inputted by a user and then check the whole statement for if the trade should
+execute.
+
+Kevin Lai
+"""
+
+from typing import List
+
+class TradeTree:
+
+    """
+    This tree combines different market conditions into a boolean tree
+    """
+
+    # condition
+    and_bool: bool
+    words: List
+
+    def __init__(self, condition):
+
+        """
+        Initializing tree with just a condition. Until others are added, only
+        the base condition matters.
+        """
+
+        self.condition = condition
+        self.and_bool = None
+        self.words = []
+
+    #TODO Please add delete condition and possible further non-theoretical stuff?
+
+    def add_words(self, sub_trade_trees: List, and_bool: bool) -> None:
+
+        """
+        Knocks self's condition down and creates a list with it and all the new
+        other trade trees. All condition checks should be at leaf level.
+        """
+
+        self.and_bool = and_bool
+
+        for sub_trade_tree in sub_trade_trees:
+            self.words.append(sub_trade_tree)
+
+        self.and_bool = and_bool
+        self.condition = None
+
+    def get_status(self) -> bool:
+
+        """
+        Goes through the tree and returns the tree's total value
+
+        >>> a = TradeTree('true')
+        >>> a.add_words([TradeTree('true'), TradeTree('false')], False)
+        >>> a.words[0].add_words([TradeTree('false'), TradeTree('true')], False)
+        >>> a.get_status()
+        True
+        """
+
+        if self.condition is not None:
+            return self.check(self.condition)
+
+        if self.and_bool:
+            for word in self.words:
+                if not word.get_status():
+                    return False
+            return True
+
+        else:
+            for word in self.words:
+                if word.get_status():
+                    return True
+            return False
+
+    def check(self, condition) -> bool:
+
+        """
+        Used to check a condition that can be specified however. This processes
+        each individual condition. It could be a string, it's own class, etc.
+        """
+
+        if self.condition == "true":
+            return True
+        return False
+
+if __name__ == '__main__':
+
+    import doctest
+    doctest.testmod()
+
+"""Catch A and B or C"""
