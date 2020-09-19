e9f3d3f14416a89a1973b872b4123f419f4b5b22
Silk8Worm <gamorfit@gmail.com>
2020-09-19 00:16:20 -0400

    Adding python GUI file.

---
+++ TT_GUI.py
@@ -0,0 +1,8 @@
+"""
+Download kivy: https://kivy.org/#download
+
+GUI designed for the trade translator based on design:
+https://www.figma.com/file/OKTVtYq4lWW2GOEMfP6k1t/Trade-Translator?node-id=17%3A0
+
+
+"""
