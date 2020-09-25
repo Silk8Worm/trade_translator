edf452f517b7fccf7cbf4a7a52a97c1cd837b2ac
Albert Cao <alberthcao@gmail.com>
2020-09-25 01:13:42 -0400

    Obtained Pricing Bar Data

--- alphavantage_gatherer.py
+++ alphavantage_gatherer.py
@@ -4,18 +4,30 @@ import alpaca_trade_api as tradeapi
 API_KEY = 'PK4W268R10HW802W8SN9'  # keys from alpaca
 SECRET_API_KEY = 'oFK1SOul4uEqwj4phpdFBAZG0CZqRraijzYQUkZH'
 
-PAPER_URL = 'https://paper-api.alpaca.markets'
-ACCOUNT_URL = '{}/v2/account'.format(PAPER_URL)
-
 MARKET_URL = 'https://data.alpaca.markets'
-QUOTE_URL = '{}/v1/last_quote/stocks/AAPL'.format(MARKET_URL)  # quote for AAPL
-STOCK = 'AAPL'
-BARS_URL = ('{}/v1/bars/minute?symbols='+STOCK+'&limit=5').format(MARKET_URL)
 
 
-def get_technical(ticker: str, indicator: str, startDate: str = None, endDate: str = None):
+def get_technical(ticker: str, indicator: str = None, startDate: str = None, endDate: str = None):
 
-    # date takes form: 'yyyy-mm-dd'
+    bars_url = ('{}/v1/bars/minute?symbols=' + ticker + '&limit=1').format(MARKET_URL)
+    r = requests.get(bars_url, headers={'APCA-API-KEY-ID': API_KEY, 'APCA-API-SECRET-KEY': SECRET_API_KEY})
+    r = r.json()
+    r = r[ticker]
+
+    # Pricing Bar Data
+    open = r[0]['o']
+    high = r[0]['h']
+    low = r[0]['l']
+    close = r[0]['c']
+    volume = r[0]['v']
+
+    print('open: \t', open)
+    print('high: \t', high)
+    print('low: \t', low)
+    print('close: \t', close)
+    print('volume: \t', volume)
+
+    pass
 
     if indicator == 'average daily volume':
         pass
@@ -71,30 +83,5 @@ def get_technical(ticker: str, indicator: str, startDate: str = None, endDate: s
     elif indicator == 'short interest':
         pass
 
-
 if __name__ == '__main__':
-
-    # print(ACCOUNT_URL)
-    # r = requests.get(ACCOUNT_URL, headers={'APCA-API-KEY-ID': API_KEY, 'APCA-API-SECRET-KEY': SECRET_API_KEY})
-    # r = r.json()
-
-    # print(QUOTE_URL)
-    # r = requests.get(QUOTE_URL, headers={'APCA-API-KEY-ID': API_KEY, 'APCA-API-SECRET-KEY': SECRET_API_KEY})
-    # r = r.json()
-
-    print(BARS_URL)
-    r = requests.get(BARS_URL, headers={'APCA-API-KEY-ID': API_KEY, 'APCA-API-SECRET-KEY': SECRET_API_KEY})
-    r = r.json()
-
-    # Prints the dictionary r
-    # for x in r:
-    #    print(x, r[x])
-
-    i = r['AAPL']
-
-    print('AAPL')
-    for x in i:
-        print(x)
-
-
-
+    get_technical('AAPL')
