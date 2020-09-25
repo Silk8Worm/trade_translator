5571b5f920e97d74c63f551b952117e5fe01fff1
Albert Cao <alberthcao@gmail.com>
2020-09-25 00:54:38 -0400

    Swapped to Alpaca

--- alphavantage_gatherer.py
+++ alphavantage_gatherer.py
@@ -1,17 +1,21 @@
-from alpha_vantage.timeseries import TimeSeries
-from alpha_vantage.techindicators import TechIndicators
-from alpha_vantage.fundamentaldata import FundamentalData
-
 import requests
+import alpaca_trade_api as tradeapi
 
-api_key = 'UD9DGIGLSXX7YVK7'  # api key from alpha vantage
+API_KEY = 'PK4W268R10HW802W8SN9'  # keys from alpaca
+SECRET_API_KEY = 'oFK1SOul4uEqwj4phpdFBAZG0CZqRraijzYQUkZH'
 
+PAPER_URL = 'https://paper-api.alpaca.markets'
+ACCOUNT_URL = '{}/v2/account'.format(PAPER_URL)
 
+MARKET_URL = 'https://data.alpaca.markets'
+QUOTE_URL = '{}/v1/last_quote/stocks/AAPL'.format(MARKET_URL)  # quote for AAPL
+STOCK = 'AAPL'
+BARS_URL = ('{}/v1/bars/minute?symbols='+STOCK+'&limit=5').format(MARKET_URL)
+
+get the vars 
 def get_technical(ticker: str, indicator: str, startDate: str = None, endDate: str = None):
 
     # date takes form: 'yyyy-mm-dd'
-
-    url = 'https://www.alphavantage.co/query?'
 
     if indicator == 'average daily volume':
         pass
@@ -32,20 +36,10 @@ def get_technical(ticker: str, indicator: str, startDate: str = None, endDate: s
         pass
 
     elif indicator == '52 week high':
-        url += "function=OVERVIEW"
-        url += "&symbol=" + ticker
-        url += "&apikey=" + api_key
-
-        r = requests.get(url)
-        return r.json()['52WeekHigh']
+        pass
 
     elif indicator == '52 week low':
-        url += "function=OVERVIEW"
-        url += "&symbol=" + ticker
-        url += "&apikey=" + api_key
-
-        r = requests.get(url)
-        return r.json()['52WeekLow']
+        pass
 
     elif indicator == 'forward dividend rate':
         pass
@@ -79,17 +73,28 @@ def get_technical(ticker: str, indicator: str, startDate: str = None, endDate: s
 
 
 if __name__ == '__main__':
-    ts = TimeSeries(key=api_key)
-    ti = TechIndicators(key=api_key)
-    fd = FundamentalData(key=api_key)
 
-    # r = requests.get('https://www.alphavantage.co/query?function=RSI&symbol=AAPL&interval=daily&time_period=14&series_type=close&apikey=UD9DGIGLSXX7YVK7')
-    # r = requests.get('https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol=GOOGL&apikey=UD9DGIGLSXX7YVK7')
-    # r = requests.get('https://www.alphavantage.co/query?function=OVERVIEW&symbol=IBM&apikey=UD9DGIGLSXX7YVK7')
-    # print(r.json())
+    # print(ACCOUNT_URL)
+    # r = requests.get(ACCOUNT_URL, headers={'APCA-API-KEY-ID': API_KEY, 'APCA-API-SECRET-KEY': SECRET_API_KEY})
+    # r = r.json()
 
-    # print(r.json['EBITDA'])
+    # print(QUOTE_URL)
+    # r = requests.get(QUOTE_URL, headers={'APCA-API-KEY-ID': API_KEY, 'APCA-API-SECRET-KEY': SECRET_API_KEY})
+    # r = r.json()
 
-    print(get_technical('IBM', '52 week high'))
-    print(get_technical('IBM', '52 week low'))
+    print(BARS_URL)
+    r = requests.get(BARS_URL, headers={'APCA-API-KEY-ID': API_KEY, 'APCA-API-SECRET-KEY': SECRET_API_KEY})
+    r = r.json()
+
+    # Prints the dictionary r
+    # for x in r:
+    #    print(x, r[x])
+
+    i = r['AAPL']
+
+    print('AAPL')
+    for x in i:
+        print(x)
+
+
 
