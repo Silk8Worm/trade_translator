7564e6171b4aab09287cd914697eb9ddd652099e
Albert Cao <alberthcao@gmail.com>
2020-09-28 01:06:44 -0400

    Changes to Data Pulled

--- alphavantage_gatherer.py
+++ alphavantage_gatherer.py
@@ -16,29 +16,28 @@ SECRET_API_KEY = 'oFK1SOul4uEqwj4phpdFBAZG0CZqRraijzYQUkZH'
 MARKET_URL = 'https://data.alpaca.markets'
 
 
-def get_technical(ticker: str, indicator: str, start_date: str = None, end_date: str = None):
-
+def get_technical(ticker: str, indicator: str, date: str = None, period: int = 1):
+    # TODO: Closing prices and volume don't match online values
     # Get Data
     time_frame = '1D'
 
     bars_url = MARKET_URL + '/v1/bars/' + time_frame + '?symbols=' + ticker
-    if start_date is not None:
-        bars_url = bars_url + '&start=' + start_date
-    if end_date is not None:
-        bars_url = bars_url + '&end=' + end_date
+    #  Obtain an additional day of data; some indicators calculate from previous day
+    bars_url = bars_url + '&limit=' + str(period+1)
+    if date is not None:
+        bars_url = bars_url + '&end=' + date
 
     r = requests.get(bars_url, headers={'APCA-API-KEY-ID': API_KEY, 'APCA-API-SECRET-KEY': SECRET_API_KEY})
     r = r.json()
     r = r[ticker]
 
-    # Get Technical
-    # Pricing Bar Data
-    open = r[len(r)-1]['o']  # [len(r)-1] references most recent day
-    high = r[len(r)-1]['h']
-    low = r[len(r)-1]['l']
-    close = r[len(r)-1]['c']
-    volume = r[len(r)-1]['v']
+    # Print obtained data
+    for d in r:
+        print(d)
 
+    pass
+
+    # Get Technical
     # Creating Dataframe from Pricing Bar Values
     d = {'Open': [], 'High': [], 'Low': [], 'Close': [], 'Volume': []}
     for day in r:
@@ -51,37 +50,37 @@ def get_technical(ticker: str, indicator: str, start_date: str = None, end_date:
     df = pd.DataFrame(data=d)
 
     if indicator == 'open':
-        return open
+        return r[len(r)-1]['o']
 
     elif indicator == 'close':
-        return close
+        return r[len(r)-1]['c']
 
     elif indicator == 'high':
-        return high
+        return r[len(r)-1]['h']
 
     elif indicator == 'low':
-        return low
+        return r[len(r)-1]['l']
 
     elif indicator == 'volume':
-        return volume
+        return r[len(r)-1]['v']
 
     elif indicator == 'BB':  # Bollinger Band
         # indicator_bb = BollingerBands(close=df["Close"], n=len(r), ndev=2)
 
-        bb_low = ta.volatility.bollinger_lband(df["Close"], n=len(r), ndev=2, fillna=True)
+        bb_low = ta.volatility.bollinger_lband(df["Close"], n=len(r)-1, ndev=2, fillna=True)
         bb_low = bb_low[len(r) - 1]
 
-        bb_high = ta.volatility.bollinger_hband(df["Close"], n=len(r), ndev=2, fillna=True)
+        bb_high = ta.volatility.bollinger_hband(df["Close"], n=len(r)-1, ndev=2, fillna=True)
         bb_high = bb_high[len(r)-1]
 
         return bb_low, bb_high
 
     elif indicator == 'ATR':  # Average True Range
-        output = ta.volatility.average_true_range(df["High"], df["Low"], df["Close"], n=len(r), fillna=True)
+        output = ta.volatility.average_true_range(df["High"], df["Low"], df["Close"], n=period, fillna=True)
         return output[len(r)-1]
 
     elif indicator == 'RSI':  # Relative Strength Index
-        output = ta.momentum.rsi(df["Close"], n=len(r), fillna=True)
+        output = ta.momentum.rsi(df["Close"], n=len(r)-1, fillna=True)
         return output[len(r)-1]
 
     elif indicator == 'OBV':  # On-Balance Volume
@@ -95,7 +94,7 @@ def get_technical(ticker: str, indicator: str, start_date: str = None, end_date:
         return macd_proper[len(r)-1], macd_signal[len(r)-1], macd_divergence[len(r)-1]
 
 if __name__ == '__main__':
-    x = get_technical('TSLA', 'MACD', '2020-09-21T09:30:00-04:00', '2020-09-25T09:30:00-04:00')
+    x = get_technical('MSFT', 'ATR', '2020-09-25T09:30:00-04:00', 14)
+    print('atr')
     print(x)
-    print('signal: ', x[1])
-    print('divergence: ', x[2])
+
