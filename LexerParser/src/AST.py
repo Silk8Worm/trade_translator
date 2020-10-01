bb15c3b4195364b7a68edd9d08ea38dafac33101
Ryan Goldberg <ryan.s.goldberg@gmail.com>
2020-10-01 16:06:33 -0400

    Added a state variable in order to change what values are used in lookup nodes in the AST. Also added build_ast() to parser which is how the parser will build an AST. There is an example of how to use this method

--- LexerParser/src/AST.py
+++ LexerParser/src/AST.py
@@ -5,63 +5,100 @@
     Python Version: 3.8
 """
 
-from typing import Union
+from typing import Union, Any
 
 
-# TODO Get data from file???
-def get_data(indicator, time=None):
-    return 50
-    # Get from IEX (
+# The current state held by the abstract syntax tree
+class ASTState:
+    def __init__(self, start_date, end_date, universe):
+        self.current_day = None
+        self.ticker = None
+        self.start = start_date
+        self.end = end_date
+        self.universe = universe
 
 
 # A abstract class for a node in the AST
 class Expr:
-    def evaluate(self):
+    def __init__(self, state: ASTState):
+        self.state = state
+
+    def evaluate(self) -> Any:
         raise NotImplementedError
 
     # def validate(self):
     #     raise NotImplementedError
 
 
-# A class which looksup an indicator value (with a possible time range)
+# A class which looks up an indicator value (with a possible time range)
 class Lookup(Expr):
-    def __init__(self, *args):
+    def __init__(self, state: ASTState, *args: list):
+        Expr.__init__(self, state)
         self.args = args
+        self.data = None
 
-    def evaluate(self):
-        return get_data(self.args)
+    def evaluate(self) -> Any:
+        # Check if the data has been pulled
+        if not self.data:
+            # Processes the number of days this indicator considers
+            if len(self.args) == 1:
+                num_days = 1
+            else:
+                # FIXME: Weeks and Months???
+                num_days = self.args[1][0]
 
-    def __str__(self):
+            # TODO: This is where Albert's data call will be made
+            print(f'\tFirst data call: getData('
+                  f'{self.state.universe},'  # A list
+                  f'{self.args[0]},'  # The indicator
+                  f'{num_days},'  # The indicator range (Ex. 10 day) otherwise its 1
+                  f'{self.state.start},'  # Start date
+                  f'{self.state.end})')  # End date
+            # FIXME: Example data based on hardcoded inputs
+            self.data = {"01/01/2020": {"APPL": 65, "TSLA": 61, "GOOG": 69},
+                         "02/01/2020": {"APPL": 62, "TSLA": 90, "GOOG": 1},
+                         "03/01/2020": {"APPL": 45, "TSLA": 65, "GOOG": 99}}
+
+        # Does the data lookup form the preinitialized list of values, self.data
+        day = self.state.current_day
+        ticker = self.state.ticker
+        return self.data.get(day).get(ticker)
+
+    def __str__(self) -> str:
         if len(self.args) > 1:
-            return f'(Lookup "{self.args[1][0]} {self.args[1][1]}: {self.args[0]}")'
+            return f'(Lookup "{self.args[1][0]} {self.args[1][1]}: ' \
+                   f'{self.args[0]} of {self.state.ticker} on ' \
+                   f'{self.state.current_day}")'
         else:
-            return f'(Lookup "{self.args[0]}")'
+            return f'(Lookup "{self.args[0]} of {self.state.ticker} on ' \
+                   f'{self.state.current_day}")'
 
 
 # A class which represents a literal float/integer value
 class Value(Expr):
-    def __init__(self, value):
+    def __init__(self, state: ASTState, value: float):
+        Expr.__init__(self, state)
         self.value = value
 
-    def evaluate(self, *args: list):
+    def evaluate(self, *args: list) -> float:
         return self.value
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'{self.value}'
 
 
 # Class which performs an operation on 2 child nodes
 class BinOp(Expr):
-    def __init__(self, fxn, left: Union[Expr, float],
+    def __init__(self, state: ASTState, fxn, left: Union[Expr, float],
                  right: Union[Expr, float]):
-        Expr.__init__(self)
-        self.left = left if isinstance(left, Expr) else Value(left)
-        self.right = right if isinstance(right, Expr) else Value(right)
+        Expr.__init__(self, state)
+        self.left = left if isinstance(left, Expr) else Value(state, left)
+        self.right = right if isinstance(right, Expr) else Value(state, right)
         self.fxn = fxn
 
-    def evaluate(self) -> list:
+    def evaluate(self) -> bool:
         return self.fxn(self.left.evaluate(), self.right.evaluate())
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'({self.left.__str__()} {self.fxn.__name__} ' \
                f'{self.right.__str__()})'
